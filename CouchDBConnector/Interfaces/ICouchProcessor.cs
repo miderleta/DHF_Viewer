c28a7a248f049bdb6767e2b5ea911fa19a19e968
Michal Derleta <miderleta@gmail.com>
2022-06-26 21:45:29 +0100

    adding user input, updating the updateDocument function
    Moved document data to main function to simulate user input.
    Changed references for all functions in CouchProcessor.cs to use new user input.
    Added a COuchDB doc revision search to UpdateDocument function so it is pulled from the database instead of being hard coded.

--- CouchDBConnector/Interfaces/ICouchProcessor.cs
+++ CouchDBConnector/Interfaces/ICouchProcessor.cs
@@ -4,11 +4,11 @@ namespace CouchDBConnector
 {
     public interface ICouchProcessor
     {
-        Task<string> CreateNewDocument();
-        Task<string> DeleteDocument();
+        Task<string> CreateNewDocument(DocumentModel documentData);
+        Task<string> DeleteDocument(DocumentModel documentData);
         string getEndpointAddress();
         Task<CouchModel> LoadCouchData();
-        Task<DocumentModel> ReadDocumentData();
-        Task<string> UpdateDocument();
+        Task<DocumentModel> ReadDocumentData(DocumentModel documentData);
+        Task<string> UpdateDocument(DocumentModel documentData);
     }
 }
