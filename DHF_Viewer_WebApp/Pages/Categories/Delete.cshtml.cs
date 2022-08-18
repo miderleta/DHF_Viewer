97f4fc70ad37d81a2bc8166fd933a5aa717aa860
Michal Derleta <miderleta@gmail.com>
2022-08-18 10:35:31 +0100

    UI updates
    Updated the link for 'update' button to direct the user to the 'Edit' page to search for the document first before editing the document data.
    Changed the 'Edit' button to 'Update' on index.html for consistency.

--- DHF_Viewer_WebApp/Pages/Categories/Delete.cshtml.cs
+++ DHF_Viewer_WebApp/Pages/Categories/Delete.cshtml.cs
@@ -1,3 +1,7 @@
+using CouchDBConnector;
+using CouchDBConnector.Interfaces;
+using CouchDBConnector.Models;
+using DHF_Viewer_WebApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +9,48 @@ namespace DHF_Viewer_WebApp.Pages.Categories
 {
     public class DeleteModel : PageModel
     {
+        [BindProperty]
+        public ReturnedData UserInput { get; set; }
+
+        public DocumentModel CouchDBConnectorDocumentModel { get; set; }
+        public String? info { get; set; }
+
         public void OnGet()
         {
+            //UserInput = new ReturnedData();
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            CouchDBConnectorDocumentModel = new DocumentModel();
+            //UserInput._id = Request.Form["UserInput._id"];
+            var documentNumber = UserInput._id;
+            CouchDBConnectorDocumentModel._id = documentNumber;
+
+            //Init HttpClient and CouchProcessor objects
+            IApiHelper client = new ApiHelper();
+            client.InitializeCouchClient();
+            ICouchProcessor dataLoader = new CouchProcessor();
+
+            //call API
+            try
+            {
+                var result = await dataLoader.DeleteDocument(CouchDBConnectorDocumentModel);
+                if (result != null)
+                {
+                    TempData["success"] = "Document Deleted successfully";
+                    info = "Document " + UserInput._id + " has been deleted.";
+                    //return RedirectToPage("delete", UserInput);
+                    return Page();
+                }
+            }
+            catch
+            {
+                TempData["success"] = "No Document found!. Please check your entry";
+            }
+            //return RedirectToPage("document_data", UserInput);
+            return Page();
         }
     }
 }
+
