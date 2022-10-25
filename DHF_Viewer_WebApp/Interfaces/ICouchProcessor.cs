0aae3e20c2667bea7e26b45892fe3faf0ba268b5
Michal Derleta <miderleta@gmail.com>
2022-10-25 16:17:43 +0100

    Removed dependency from CouchDBConnector project.
    Moved all models, interfaces and utility files to the web app project.
    Fixed all the namespaces across all files.

---
+++ DHF_Viewer_WebApp/Interfaces/ICouchProcessor.cs
@@ -0,0 +1,18 @@
+﻿using DHF_Viewer_WebApp.Models;
+using System.Collections;
+
+namespace DHF_Viewer_WebApp.Interfaces
+{
+    public interface ICouchProcessor
+    {
+        Task<string> CreateNewDocument(DocumentModel documentData);
+        Task<string> DeleteDocument(DocumentModel documentData);
+        string getEndpointAddress();
+        Task<CouchModel> LoadCouchData();
+        Task<DocumentModel> ReadDocumentData(DocumentModel documentData);
+        Task<string> UpdateDocument(DocumentModel documentData);
+        Task<ReportModel> ReportAllDocuments();
+        Task<ReportModel> ReportByProduct(DocumentModel documentData);
+        Task<ReportModel> SearchByDocumentName(DocumentModel documentData);
+    }
+}
---
+++ DHF_Viewer_WebApp/Models/PayloadUpdateDocumentModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DHF_Viewer_WebApp.Models
+{
+    public class PayloadUpdateDocumentModel
+    {
+        public string? Title { get; set; }
+        public string? _rev { get; set; }
+        public string? Type { get; set; }
+        public string? Revision { get; set; }
+        public string? Product { get; set; }
+    }
+}
